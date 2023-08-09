e434bedc7118dd836c0b57b4d4a264c3fa326354
p0chitaa <retr0.spekktion@gmail.com>
2023-08-09 08:13:38 -0400

    reworks calculator, changes from grid to form with working mojolicious endpoints

--- calculator.pl
+++ calculator.pl
@@ -2,23 +2,56 @@
 use Mojolicious::Lite -signatures;
 use Mojo::DOM;
 
-get '/result' => sub ($c) {
-    $c->ua->get('localhost:3000' => sub ($ua, $tx) {
-        $c->render(data => $tx->result->dom->at('p')->text);
-    });
-};
-
 any '/' => sub ($c) {
     my $result = 0;
-    $c->render(template => 'calc', result => $result);
+    $c->render(template => 'newcalc', result => $result);
+};
+
+post '/calculate' => sub ($c) {
+    my $result = 0;
+    my $num1 = $c->param('num1');
+    my $num2 = $c->param('num2');
+    my $operation = $c->param('operation');
+
+    if($operation eq "add") {
+        $result = $num1 + $num2;
+    } elsif($operation eq "subtract") {
+        $result = $num1 - $num2;
+    } elsif($operation eq "multiply") {
+        $result = $num1 * $num2;
+    } elsif($operation eq "divide") {
+        $result = $num1 / $num2;
+    }
+
+    $c->render(template => 'newcalc', result => $result);#, text => "Num 1: $num1\nNum 2: $num2\nOperator: $operation");
 };
 
 app->start;
 __DATA__
 
-@@ calc.html.ep
+@@ newcalc.html.ep
 
-%= javascript 'https://unpkg.com/axios/dist/axios.min.js'
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<div class="calculator">
+    <form id="calcForm" action="/calculate" method="post">
+        <input type="number" name="num1" placeholder="Enter first number">
+        <select name="operation">
+            <option value="add">+</option>
+            <option value="subtract">-</option>
+            <option value="multiply">*</option>
+            <option value="divide">/</option>
+        </select>
+        <input type="number" name="num2" placeholder="Enter second number">
+        <input type="submit" value="Calculate">
+    </form>
+    <p>Result: <span id="result"><%= $result %></span></p>
+</div>
+</body>
+</html>
+
+@@ calc.html.ep
 
 <!DOCTYPE html>
 <html lang="en">
@@ -43,7 +76,7 @@ __DATA__
         padding: 20px;
         width: 90%;
         background-color: rgba(50, 50, 50, 0.8);
-        height: 25%;
+        height: 10%;
         border-radius: 200px;
     }
 
@@ -67,7 +100,7 @@ __DATA__
     }
     
     .calc-button {
-        border: 2px solid rgba(230, 230, 230, 0.2);
+        border: 2px solid rgba(230, 230, 230, 0);
         padding-top: 20%;
         padding-bottom: 20%;
         text-align: center;
@@ -89,7 +122,7 @@ __DATA__
 
     #zero-button {
         grid-area: 5 / 1 / span 1 / span 2;
-        border: 2px solid rgba(230, 230, 230, 0.2);
+        border: 2px solid rgba(230, 230, 230, 0);
         padding: 10%;
         font-family: 'Inter', sans-serif;
         font-size: 3rem;
@@ -110,7 +143,7 @@ __DATA__
 
     #dot {
         grid-area: 5 / 3;
-        border: 2px solid rgba(230, 230, 230, 0.2);
+        border: 2px solid rgba(230, 230, 230, 0);
         padding: 20%;
         font-family: 'Inter', sans-serif;
         font-size: 3rem;
